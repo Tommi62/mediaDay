9c05a005be2cd0057033e4b6a93bb199af6fa47e
Tommi62 <tommi.vainio21@gmail.com>
2021-09-27 10:15:03 +0300

    cleaner

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import './App.css';
 
-import { Switch, Route, Link } from "react-router-dom";
-import { Nav, Navbar } from 'react-bootstrap';
+import { Switch, Route } from "react-router-dom";
 
+import Navigation from './components/Navigation'
 import About from './pages/About'
 import PromoVideo from './pages/PromoVideo';
 import EventPage from './pages/EventPage';
 import SchedulePage from './pages/SchedulePage';
-import logo from './logo.svg';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
 
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">
-          <img
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-
-        <Nav defaultActiveKey="/home" className="me-auto">
-          <Nav.Item>
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/schedule">Schedule</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/promovideo">Tapahtuman promo</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/event/mediaday-123">Mediaday event linkki</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/event/mediaday-567">Mediaday event 567</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
+      <Navigation />
 
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -60,20 +29,7 @@ function App() {
         </Route>
         <Route path="/event/:id" children={<EventPage />} />
         <Route path="/">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <Home />
         </Route>
       </Switch>
     </div>

dccb574f1c4060408fd5648e91d723dcb617a0bc
Tommi62 <tommi.vainio21@gmail.com>
2021-10-06 16:54:08 +0300

    Stream start date matters now

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -2,6 +2,7 @@ import '../css/VideoPage.css'
 
 import React, { useEffect, useState } from 'react';
 import { Col, Container, ListGroup, Row } from 'react-bootstrap';
+import moment from 'moment';
 import Video from '../components/Video';
 import VideoButton from '../components/VideoButton';
 
@@ -14,6 +15,8 @@ const VideoPage = () => {
     const [mediaType, setMediaType] = useState('');
     const [show, setShow] = useState(false);
     const [isStreamLive, setIsStreamLive] = useState(false);
+    const [isItStreamTime, setIsItStreamTime] = useState(false);
+    const [streamStatus, setStreamStatus] = useState('');
 
     useEffect(() => {
         try {
@@ -49,12 +52,67 @@ const VideoPage = () => {
         }
     }, [isStreamLive]);
 
+    useEffect(() => {
+        try {
+            const now = moment().format("YYYY-MM-DD HH:mm:ss");
+            const streamStartTime = mediaData.media[0].startTime;
+            const compareStartTime = moment(streamStartTime, "YYYY-MM-DD HH:mm:ss").diff(now, "seconds");
+
+            const streamEndTime = mediaData.media[0].endTime;
+            const compareEndTime = moment(streamEndTime, "YYYY-MM-DD HH:mm:ss").diff(now, "seconds");
+            const endTimeWithSafeMargin = compareEndTime + 7200;
+            console.log('END', endTimeWithSafeMargin);
+            if (compareStartTime <= 0 && endTimeWithSafeMargin >= 0) {
+                if (compareEndTime <= 0) {
+                    setStreamStatus('overTime')
+                } else {
+                    setStreamStatus('showTime');
+                }
+                setIsItStreamTime(true);
+            } else if (compareStartTime > 0) {
+                setIsItStreamTime(false);
+                setStreamStatus('inFuture');
+            } else {
+                setIsItStreamTime(false);
+                setStreamStatus('over');
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+    }, []);
+
     return (
         <div>
             <Container>
                 <Row>
                     <Col>
-                        <h2 className="display-5">Tapahtumaan liittyvät videot löytyvät täältä</h2>
+                        {streamStatus === '' && ''}
+                        {streamStatus === 'inFuture' &&
+                            <h2 className="display-5 videoPageTitle">Striimi starttaa tällä sivulla {mediaData.media[0].startTime}</h2>
+                        }
+                        {streamStatus === 'showTime' &&
+                            <div>
+                                {isStreamLive ? (
+                                    <h2 className="display-5 videoPageTitle">Striimi on käynnissä</h2>
+                                ) : (
+                                    <h2 className="display-5 videoPageTitle">Striimi alkaa pian</h2>
+                                )
+                                }
+                            </div>
+                        }
+                        {streamStatus === 'overTime' &&
+                            <div>
+                                {isStreamLive ? (
+                                    <h2 className="display-5 videoPageTitle">Striimi on käynnissä</h2>
+                                ) : (
+                                    <h2 className="display-5 videoPageTitle">Striimi on päättynyt</h2>
+                                )
+                                }
+                            </div>
+                        }
+                        {streamStatus === 'over' &&
+                            <h2 className="display-5 videoPageTitle">Tapahtumaan liittyvät videot</h2>
+                        }
                     </Col>
                 </Row>
                 {videoStream !== {} &&
@@ -63,7 +121,15 @@ const VideoPage = () => {
                             <Col lg={9} style={{ marginLeft: '15px' }}>
                                 <Row>
                                     {mediaType !== '' &&
-                                        <Video url={videoStream.url} type={videoStream.streamVideoType} mediaType={mediaType} setIde={setIde} setShow={setShow} setIsStreamLive={setIsStreamLive} />
+                                        <Video
+                                            url={videoStream.url}
+                                            type={videoStream.streamVideoType}
+                                            mediaType={mediaType}
+                                            setIde={setIde}
+                                            setShow={setShow}
+                                            setIsStreamLive={setIsStreamLive}
+                                            isItStreamTime={isItStreamTime}
+                                        />
                                     }
                                 </Row>
                                 <Row>
@@ -81,7 +147,15 @@ const VideoPage = () => {
                             <Col lg={9} style={{ marginLeft: '15px' }}>
                                 <Row style={{ display: 'none' }}>
                                     {mediaType !== '' &&
-                                        <Video url={videoStream.url} type={videoStream.streamVideoType} mediaType={mediaType} setIde={setIde} setShow={setShow} setIsStreamLive={setIsStreamLive} />
+                                        <Video
+                                            url={videoStream.url}
+                                            type={videoStream.streamVideoType}
+                                            mediaType={mediaType}
+                                            setIde={setIde}
+                                            setShow={setShow}
+                                            setIsStreamLive={setIsStreamLive}
+                                            isItStreamTime={isItStreamTime}
+                                        />
                                     }
                                 </Row>
                                 <Row style={{ display: 'none' }}>
